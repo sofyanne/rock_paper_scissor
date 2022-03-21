9ab9c0ca7cece90caee1679ee3f9ce0c1320b108
Sofyanne Badir <s.badir@outlook.com>
2022-03-21 14:03:33 +0100

    Refactoring de la fonctionnalité choix utilisateur

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const qSElt = (nameElt) => document.querySelector(`${nameElt}`);
 const choice_p1 = qSElt(".battle_child.player-1");
 const choice_p2 = qSElt(".battle_child.player-2");
@@ -9,30 +10,25 @@ const scores_player1 = qSElt(".player-1_scores");
 const scores_player2 = qSElt(".player-2_scores");
 const reset_button = qSElt(".manage_game-reset");
 
-const setUserChoice = (choice) => {
-  choice_p1.textContent = choice;
-  choice_p1.style.display = "block";
-};
-
-const setComputerChoice = (choice) => {
-  choice_p2.textContent = choice;
-  choice_p2.style.display = "block";
+const setChoice = (choice, elt) => {
+  elt.textContent = choice;
+  elt.style.display = "block";
 };
 
 const app = () => {
   //Tester event delegation.
   let userChoice = "";
   rock_button.addEventListener("click", (e) => {
-    setUserChoice(e.target.innerText);
-    setComputerChoice(e.target.innerText);
+    setChoice(e.target.innerText, choice_p1);
+    setChoice(e.target.innerText, choice_p2);
   });
   paper_button.addEventListener("click", (e) => {
-    setUserChoice(e.target.innerText);
-    setComputerChoice(e.target.innerText);
+    setChoice(e.target.innerText, choice_p1);
+    setChoice(e.target.innerText, choice_p2);
   });
   scissors_button.addEventListener("click", (e) => {
-    setUserChoice(e.target.innerText);
-    setComputerChoice(e.target.innerText);
+    setChoice(e.target.innerText, choice_p1);
+    setChoice(e.target.innerText, choice_p2);
   });
 };
 
